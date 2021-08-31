f1f34c02151242a8bdb54d3405eea38d0f3bc62f
oliviergifss <89310863+oliviergifss@users.noreply.github.com>
2021-08-31 21:16:00 +0200

    Update jason_list.js

--- jason_list.js
+++ jason_list.js
@@ -462,19 +462,8 @@ var gifs = [
 	{
         modelo:100,
         img: "https://i.postimg.cc/1XzhwQmS/100.gif"
-    },
-	
-	{ :,
-      
-        img: "https://i.postimg.cc/02F8D7P0/coollogo-com-9411354.png"
     }
 	
-	
-	
-
-
-        
-
     	
 ];
 
