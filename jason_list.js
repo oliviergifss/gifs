4a08a1f68f680c8548526bc76380419f73d7a2d7
oliviergifss <89310863+oliviergifss@users.noreply.github.com>
2021-09-06 20:58:32 +0200

    Update jason_list.js

--- jason_list.js
+++ jason_list.js
@@ -17,6 +17,36 @@ var modelos = [
         img: "https://i.postimg.cc/zvxBXsns/3.gif"
     },
     {
+        modelo:05,
+        img: "https://i.postimg.cc/fRLdXKy7/5.gif"
+    },
+    {
+        type: "simples",
+        model: "06",
+        img: "https://i.postimg.cc/wTXj4syt/6.gif"
+    },
+    {
+       type: "simples",
+        model: "07",
+        img: "https://i.postimg.cc/6pz5tJht/7.gif"
+    },
+    
+	{
+        type: "simples",
+        model: "08",
+        img: "https://i.postimg.cc/g2GkCk8g/8.gif"
+    },
+	{
+        type: "simples",
+        model: "09",
+        img: "https://i.postimg.cc/MK2GYYT2/9.gif"
+    },
+    {
+        type: "simples",
+        model: "10",
+        img: "https://i.postimg.cc/L4ZXfWm3/10.gif"
+    },
+    {
         type: "simples",
         model: "04",
         img: "https://i.postimg.cc/3xswRCvx/4.gif"
